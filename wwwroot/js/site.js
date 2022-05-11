205d9f2ba410fa335364a7a929d6ffe4a74c5e96
FagnerMenezes <fsantosmenezes@gmail.com>
2022-05-11 16:25:45 -0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function ocultaDiv(){
-    
-     let div= document.getElementById('div-privacidade_');
-     div.style.display='none';
+function ocultaDiv() {
 
-}  
+     let div = document.getElementById('div-privacidade_');
+     div.style.display = 'none';
+
+}
 
 
-
+$(document).ready(function () {
+     $(function () {       
+          document.querySelector("link[rel='shortcut icon']").href = "/wwwroot/images/favicon_io (1)/favicon.ico";
+         // document.querySelector("link[rel='shortcut icon']").href = "/wwwroot/images/logo.wwsoldas.png";
+     });
+});
